958d38e16127a850d27fd5af71b1fade71572a6d
Ron Talledo <Rontalledo@gmail.com>
2024-09-19 22:48:56 +0800

    connect to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 import userRouter from "./routes/userRoutes";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
+app.use(express.json());
 app.use("/", userRouter);
 
+console.log(process.env.MONGO_URI);
+
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${port}`);
 });
