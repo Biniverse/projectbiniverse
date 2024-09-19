f264f3c211e082a7505eca41baf8e0bb6acb53f4
CEBUPACIFICAIR\ssastephen <Stephen.Abueva@cebupacificair.com>
2024-09-19 17:27:30 +0800

    Remove console logs

--- frontend/src/controllers/testcontroller.tsx
+++ frontend/src/controllers/testcontroller.tsx
@@ -8,9 +8,6 @@ const testService = {
   getData: async () => {
     try {
       const response = await axios.get(`${API_URL}/test`);
-      console.log("Success");
-      console.log("Type", typeof response);
-      console.log(response.data);
       return response.data;
     } catch (error) {
       console.error("Error fetching data:", error);
