972033be0ff2ef228bed7bf7fbb1864dc1f57218
Ron Talledo <82923911+22tofu@users.noreply.github.com>
2024-09-19 22:54:51 +0800

    connect to database (#14)

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -1,9 +1,30 @@
 // src/services/helloService.ts
 import { Request, Response } from "express";
 import { getApiGreetings } from "../service/userService";
+import User from "../model/userModel";
+
 export const getHelloWorld = (req: Request, res: Response): void => {
   const message: string = getApiGreetings();
   res.status(200).json({
     message: message,
   });
 };
+
+export const createUser = async (req: Request, res: Response) => {
+  const user = req.body;
+
+  const newUser = new User(user);
+
+  try{
+    await newUser.save();
+    res.status(201).json({
+      status: 201,
+      data: newUser
+    })
+  }catch(error){
+		res.status(500).json({
+      success: false, 
+      message: "Server Error" 
+    });
+  }
+}
