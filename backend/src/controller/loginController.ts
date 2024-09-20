25ad7ef02c351127c31a27cc3158c2f0b37c4ae4
CEBUPACIFICAIR\ssastephen <Stephen.Abueva@cebupacificair.com>
2024-09-20 15:20:23 +0800

    added docstring

--- backend/src/controller/loginController.ts
+++ backend/src/controller/loginController.ts
@@ -5,6 +5,20 @@ const USER_NAME = "user";
 const EMAIL = "user@user.com";
 const PASSWORD = "testpassword";
 
+/**
+ * The function `loginUser` handles user login authentication based on provided credentials and app
+ * key.
+ * @param {Request} req - The `req` parameter in the `loginUser` function represents the request
+ * object, which contains information about the HTTP request made to the server. It includes details
+ * such as headers, body, parameters, and more. In this function, `req` is of type `Request`, which is
+ * typically provided
+ * @param {Response} res - The `res` parameter in the `loginUser` function is the response object that
+ * will be used to send responses back to the client making the request. It is an instance of the
+ * `Response` class from the Express.js framework. This object allows you to send HTTP responses with
+ * status codes, headers
+ * @returns The `loginUser` function returns JSON responses based on the conditions checked in the
+ * code:
+ */
 export const loginUser = async (req: Request, res: Response) => {
   const message: string = "Login Success";
   const unauthorized: string = "Invalid Credentials. Try again";
