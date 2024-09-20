cf2b7d3aa2633845c7d03cdff390eafd417519ad
CEBUPACIFICAIR\ssastephen <Stephen.Abueva@cebupacificair.com>
2024-09-20 14:07:40 +0800

    added for signin

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 // src/routes/helloRoute.ts
 import { Router } from "express";
 import { getHelloWorld, createUser } from "../controller/userController";
+import { loginUser } from "../controller/loginController";
 
 const userRouter = Router();
 
 userRouter.get("/test", getHelloWorld);
 
 userRouter.post("/users", createUser);
+
+userRouter.post("/login", loginUser);
 
 export default userRouter;
