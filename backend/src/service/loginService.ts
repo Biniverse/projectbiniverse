c8315758cf77c04731094aacc5862ab8fab708e8
CEBUPACIFICAIR\ssastephen <Stephen.Abueva@cebupacificair.com>
2024-09-23 08:35:38 +0800

    Create a placeholder function for loginService

---
+++ backend/src/service/loginService.ts
@@ -0,0 +1,9 @@
+// Login Service placeholder
+
+/**
+ * The function LoginServicePlaceholder() returns a placeholder HTML element for a sign-in service.
+ * @returns A string containing a placeholder message for a signin service is being returned.
+ */
+function LoginServicePlaceholder() {
+  return "<p>This is only a placeholder for signin service </p>";
+}
