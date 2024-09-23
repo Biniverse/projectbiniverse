f14479292a2f8eb649a3c88a4c9e58fb4e664cfe
John Patrick Ryan D. Mandal <JohnPatrickRyan.Mandal@cebupacificair.com>
2024-09-23 16:30:01 +0800

    update about us

--- frontend/src/components/About/index.tsx
+++ frontend/src/components/About/index.tsx
@@ -3,66 +3,64 @@ import { TeamMember } from "../../shared/types";
 const teamMembers: TeamMember[] = [
   {
     name: "Edward Joseph A. Habon",
-    role: "FullSnack Developer",
-    bio: "likes to eat pancit",
-    imageUrl: "/images/images.jpg",
-  },
-  {
-    name: "Ester S. Hubahib",
-    role: "FullSnack Developer",
-
-    bio: "likes to eat pancit",
-    imageUrl: "/images/images.jpg",
+    role: "Fullstack Developer",
+    bio: "Maybe we got lost in translation",
+    imageUrl: "/images/edward.jpg",
   },
   {
     name: "John Patrick Ryan D. Mandal",
-    role: "FullSnack Developer",
-
-    bio: "likes to eat pancit",
+    role: "Fullstack Developer",
+    bio: "Maybe I asked for too much",
+    imageUrl: "/images/ptrck.jpg",
+  },
+  {
+    name: "Ron Dave Fredrick N. Talledo",
+    role: "Fullstack Developer",
+    bio: "But maybe this thing was a masterpiece",
+    imageUrl: "/images/ron.jpg",
+  },
+  {
+    name: "Stephen S. Abueva",
+    role: "Fullstack Developer",
+    bio: "til you tore it all up",
     imageUrl: "/images/images.jpg",
   },
   {
     name: "Jovany S. Opelina",
-    role: "FullSnack Developer",
-
-    bio: "likes to eat pancit",
-    imageUrl: "/images/images.jpg",
+    role: "Fullstack Developer",
+    bio: "Running scared, I was there",
+    imageUrl: "/images/jovany.jpg",
   },
   {
-    name: "Stephen S. Abueva",
-    role: "FullSnack Developer",
-
-    bio: "likes to eat pancit",
-    imageUrl: "/images/images.jpg",
-  },
-  {
-    name: "Ron Dave N. Talledo",
-    role: "FullSnack Developer",
-
-    bio: "likes to eat pancit",
+    name: "Ester S. Hubahib",
+    role: "Fullstack Developer",
+    bio: "I remember it all too well",
     imageUrl: "/images/images.jpg",
   },
 ];
+
 export const AboutUsComponent = () => {
   return (
     <section className="mb-5">
       <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold text-gray-800 mb-8">About Us</h2>
-        <p className="text-lg text-gray-600 mb-12">
-          We are a team of passionate individuals dedicated to the side quests
-          grind.
-        </p>
+        <h2 className="text-5xl font-extrabold text-gray-800 mb-8">
+          Meet the Team
+        </h2>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {teamMembers.map((member) => (
             <div
               key={member.name}
               className="bg-white rounded-lg shadow-md overflow-hidden"
             >
-              <img
-                className="w-full h-56 object-cover"
-                src={member.imageUrl}
-                alt={member.name}
-              />
+              <div className="w-full h-64 overflow-hidden">
+                <img
+                  className="w-full h-full object-cover"
+                  src={member.imageUrl}
+                  alt={member.name}
+                />
+              </div>
+
               <div className="p-6">
                 <h3 className="text-xl font-semibold text-gray-800 mb-2">
                   {member.name}
