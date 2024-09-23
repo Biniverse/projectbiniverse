2b050e9bb682a325cb3714ed2d1cb9899ed188cc
noobgrammer <stephen.abueva@gmail.com>
2024-09-23 13:26:36 +0800

    Add TODO

--- backend/src/service/userService.ts
+++ backend/src/service/userService.ts
@@ -2,3 +2,5 @@ export const getApiGreetings = () => {
   let message: string = "Greetings from Biniverse API v1.0.0";
   return message;
 };
+
+// TODO: If DB data is available create service that will connect and communicate with the DB service
