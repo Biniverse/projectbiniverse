52c97309a42d66d8e37550e711ccf5f696969c16
John Patrick Ryan D. Mandal <JohnPatrickRyan.Mandal@cebupacificair.com>
2024-09-24 18:12:13 +0800

    [BE]: Deployment build

--- backend/dist/model/userModel.js
+++ backend/dist/model/userModel.js
@@ -4,16 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const enums_1 = require("../shared/enums");
 const userSchema = new mongoose_1.default.Schema({
-    name: {
+    firstName: {
         type: String,
-        required: true
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    contact: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
     },
     role: {
         type: String,
-        required: true
+        enum: Object.values(enums_1.ROLES),
+        required: true,
     },
 });
-const User = mongoose_1.default.model('User', userSchema);
+const User = mongoose_1.default.model("User", userSchema);
 exports.default = User;
 //# sourceMappingURL=userModel.js.map
