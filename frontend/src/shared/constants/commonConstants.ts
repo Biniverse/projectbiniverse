21bf0d7daf393dd3eb7a61cd80ea5f7d773ed160
Jefone Gorme <jefonegorme@gmail.com>
2024-09-24 17:03:49 +0800

    [FE] Added constants for centralized usage of magic strings
    This commit is to have centralized usage of magic strings on the application.

---
+++ frontend/src/shared/constants/commonConstants.ts
@@ -0,0 +1,8 @@
+export class CommonConstant {
+  public static readonly MEET_THE_TEAM = "Meet the Team";
+  public static readonly COPYRIGHT_TEXT = "© 2024 BiniVerse. All rights reserved.";
+  public static readonly BINIVERSE_TITLE = "Biniverse";
+  public static readonly PAGE_NOT_FOUND_TEXT = "404, Page not found";
+  public static readonly PRIVACY_POLICY = "Privacy Policy"
+  public static readonly TERMS_OF_SERVICE = "Terms of Service";
+}
