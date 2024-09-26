0dd477f78e57f646c1e60dbae2356f5df1f0b1bb
UGO-2024-LOW <jovany.opelina@gmail.com>
2024-09-26 20:34:57 +0800

    Add reusable and customizable calendar component

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -5,6 +5,8 @@ import DrawerComponent from "../Sidebar";
 import AnnouncementComponent from "../Announcement";
 import ProfileDetailsComponent from "../ProfieDetails";
 import { CardComponent } from "../../shared/components/CardNav";
+import { TimeLogsComponent } from "../TimeLogs";
+import { CommonConstant } from "../../shared/constants/commonConstants";
 
 const DashboardComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,13 +14,20 @@ const DashboardComponent = () => {
   const toggleDrawer = () => {
     setIsOpen(!isOpen);
   };
+
+  const commonConstant = CommonConstant;
+
   return (
     <>
       <CardComponent>
         <DrawerComponent />
         <ProfileDetailsComponent />
         <AnnouncementComponent />
-        <Datepicker inline />
+        {/* <Datepicker inline /> */}
+        <TimeLogsComponent
+          isCalendarOnly={false}
+          logData={commonConstant.MOCK_DATA_TIME_LOGS}
+        />
       </CardComponent>
     </>
   );
